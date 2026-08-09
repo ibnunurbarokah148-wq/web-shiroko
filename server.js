ac2199451a84dac79c126a3f51e282733f56bc41
ibnunurbarokah148-wq <ibnunurbarokah148@gmail.com>
2026-08-09 20:54:14 +0700

    fix: pass title variable to pixai-api view

--- server.js
+++ server.js
@@ -464,7 +464,7 @@ app.get('/', async (req, res) => {
 
 app.get('/projects', (req, res) => res.render('projects', { title: 'Projects' }));
 app.get('/docs', (req, res) => res.render('docs', { title: 'Documentation' }));
-app.get('/pixai-api', (req, res) => res.render('pixai-api', { currentPath: '/pixai-api' }));
+app.get('/pixai-api', (req, res) => res.render('pixai-api', { title: 'PixAI Web Auth', currentPath: '/pixai-api' }));
 
 app.get('/status', async (req, res) => {
     let botOnline = false;
